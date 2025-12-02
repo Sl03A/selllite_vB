f2627a456a976441a0f724087a4798c1d3e8111f
Sl03A <slozwslozww@proton.me>
2025-12-02 11:34:45 +0100

    Delete selllite_vB directory

--- selllite_vB/server/db.js
+++
@@ -1,8 +0,0 @@
-import Database from 'better-sqlite3';
-import fs from 'fs';
-import path from 'path';
-const DB_PATH = process.env.DB_PATH || path.join(process.cwd(),'server','data.sqlite');
-const db = new Database(DB_PATH);
-const initSql = fs.readFileSync(path.join(process.cwd(),'server','migrations','init.sql'),'utf8');
-db.exec(initSql);
-export default db;
